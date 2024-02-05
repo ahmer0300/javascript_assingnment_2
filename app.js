e93d63ae7f08bc35fe2e51210142fdb393394175
MIRZA AHMER BAIG <ahmermirza857@gmail.com>
2024-02-05 23:33:38 +0500

    JAVASCRIPT_ASSINGNMENT_2

---
+++ app.js
@@ -0,0 +1,136 @@
+//  ASSINGMENT NO 2
+
+// Question no 1
+// part (a)
+// var a = 10;
+// var b = ++a;
+// document.write (b + "<br>")
+
+// part (b)
+
+// var a = 11;
+// var b = ++a;
+// document.write  (b + "<br>");
+
+// part (c)
+
+// var a = 12;
+// var b = --a;
+// document.write (b + "<br>");
+
+// part (d)
+
+// var a =11;
+// var b = --a;
+// document.write (b+ "<br>" )
+
+
+// Question no 2
+
+// part (a)
+// var a = 2;
+// var b = 1;
+// var c = --a - --b + ++b + b--;  
+    
+
+// alert(c)
+
+// part (b)
+
+// (1) --a; = 1
+// (2) --a - --b; = 1
+// (3) --a - --b + ++b; = 2
+// (3) --a - --b + ++b + b--; = 3
+
+
+// Question no 3
+
+// var name = prompt("what is your name");
+// if(name =="Hussain"  )
+// {
+// alert("Assalam o Alaikum Sir ")
+// }
+
+
+// Question no 4
+
+
+// var table = +prompt ("Enter a Number")
+// var table5 = 5;
+
+
+// if (table === table5){
+
+//     for (var i = 1; i<=10; i++ )
+//     {
+//         document.write(table + " " + "x" + " "  + i + " " + "=" + " " + i*table5 + "<br>"  )
+
+//     }
+// }
+// else {
+//     alert ("Please Enter Correct Number")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question no 5
+
+// part (a)
+
+// var subject1 = prompt ("Enter first Subject Name");
+// var subject2 = prompt ("Enter Second Suject Name");
+// var subject3 = prompt ("Enter Third Suject Name");
+
+// part (b)
+
+// var totalMarks = 100;
+
+
+
+// document.write ("Total Marks for" + subject1 + " " + totalMarks + "<br>")
+// document.write ("Total Marks for" + subject2 + " " + totalMarks + "<br>")
+// document.write ("Total Marks for" + subject3 + " " + totalMarks + "<br>")
+
+// part (c)
+
+// var marks1= +prompt("Total marks for" + subject1 )
+// var marks2= +prompt("Total marks for" + subject2 )
+// var marks3= +prompt("Total marks for" + subject3 )
+
+// document.write("Obtained marks for " + subject1 + " " + "is" + " " + marks1 + "<br>")
+// document.write("Obtained marks for " + subject2 + " " + "is" + " " + marks2 + "<br>")
+// document.write("Obtained marks for " + subject3 + " " + "is" + " " + marks2 + "<br>")
+
+// part (d)
+
+// var totalObtainedMarks= marks1 + marks2 + marks3;
+
+// document.write ("Total obtained marks is" + " " + totalObtainedMarks + "<br>")
+
+// part (e)
+
+// var percentage = (totalObtainedMarks / (totalMarks*3))*100;
+
+
+// document.write("Percentage" + " " + percentage + "<br>")
